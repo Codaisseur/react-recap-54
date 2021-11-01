5d58a3f973db7f1cbc607bb2336fdd61b88ce76d
Matias Garcia <matiasigarcia91@gmail.com>
2021-11-01 16:37:36 +0100

    set up router and pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
+import HomePage from "./pages/Homepage";
+import LoginPage from "./pages/Loginpage";
+
+const NavBar = () => {
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ marginRight: 30 }}>
+        <Link to='/'>Home</Link>
+      </div>
+      <div>
+        <Link to='/login'>Login</Link>
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <NavBar />
+      <Switch>
+        <Route path='/login' component={LoginPage} />
+        <Route path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
